0158a009d4271cf0655749d569a2e8d522eaa86d
Logan Herrera <lyoyo@yahoo.com>
2020-03-21 15:05:54 -0600

    - LH Added the probability display of when you actually land on all heads or all tails.

--- flipcoinClass.py
+++ flipcoinClass.py
@@ -12,20 +12,28 @@ from fractions import *
 #Class definition that takes user input
 class FlipCoin:
 
+#Instance class variables
     def __init__(self, cases, number):
         self.cases = cases
         self.number = number
 
 #Print results of each case to the user
-#May update later to give probability of HEADS or TAILS
     @classmethod
     def giveResults(self, cases, number):
         for i in range(cases):
             answer = self.cointoss(number)
-            print "Case: ", i+1
-            print "Coin flipped ", number, " times."
-            print "Heads:Tails " , answer
-            print "\n"
+            print ("Case: ", i+1)
+            print ("Coin flipped ", number, " times.")
+            print ("Heads:Tails " , answer)
+            if 0 in answer:
+                print("Congratulations!! You got the ", self.probability(number), " chance of getting this!")
+                if answer[0] == 0:
+                    print("Lucky Coin all flips landed on Tails!")
+                elif answer[1] == 0:
+                    print("Lucky Coin all flips landed on Heads!")
+                else:
+                    print("\n")
+            print ("\n")
 
 #Simulate the coin Flip and record results in a list
     @classmethod
@@ -39,11 +47,20 @@ class FlipCoin:
                 recordList.append('T')
         return (recordList.count('H'), recordList.count('T'))
 
+#Calculate the probability of getting the same flip everytime
+    @classmethod
+    def probability(self, number):
+        chance = Fraction(1/2)
+        prob = chance
+        for i in range(number):
+            prob *= chance
+        return prob
+
 #Main function to take input from user to start monte-carlo run
 def main():
     cases = int(input("How many cases? "))
     number = int(input("How many times do we flip the coin?"))
-    fC = FlipCoin(cases, number,)
+    fC = FlipCoin(cases, number)
     fC.giveResults(cases, number)
 
 #Creating modularity independence
